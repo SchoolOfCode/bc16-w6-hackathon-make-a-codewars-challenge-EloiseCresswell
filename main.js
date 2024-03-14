01970c4f5049169aa7383f07ffff3060b3943f68
Leigh and El <leighandel@Leighs-MacBook-Pro.local>
2024-03-14 18:02:20 +0000

    added tests 5 and 6 - this addedf lines 33-35 and 29-30 in the function

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
  *
  *Please note, with this dog, you will also be loosing money, so they will always cost you!
  *However, take into consideration what you may need to change if there was only 1 item *recieved or if no items were received...
- *
+ *If the cost is less than £1, for example 20p, the answer string should still be in pound format i.e. £0.20.
  * A few examples:
  *    `dogMonthlyCost(['sticks', 2 , 'balls', 100, 'Ivy'])` should return "My dog Ivy has 2 sticks and 100 balls. This month they have cost me £10"
  *    `dogMonthlyCost(['sticks', 0, 'balls', 50, 'Steve'])` should return "My dog Steve has 0 sticks and 50 balls. This month they have cost me £10"
@@ -19,15 +19,22 @@
 export function dogMonthlyCost(array) {
   // Good luck!
   let sticks = "sticks";
+  let balls = "balls";
   const numberSticks = array[1];
   if (numberSticks === 1) {
     sticks = "stick";
   }
   const costSticks = numberSticks * 5;
   const numberBalls = array[3];
+  if (numberBalls === 1) {
+    balls = "ball";
+  }
   const costBalls = numberBalls * 0.2;
-  const cost = costBalls - costSticks;
-  return `My dog ${array[4]} has ${numberSticks} ${sticks} and ${numberBalls} balls. This month they have cost me £${cost}`;
+  let cost = costBalls - costSticks;
+  if (!Number.isInteger(cost)) {
+    cost = cost.toFixed(2);
+  }
+  return `My dog ${array[4]} has ${numberSticks} ${sticks} and ${numberBalls} ${balls}. This month they have cost me £${cost}`;
 }
 
 //['sticks', 2 , 'balls', 100, 'Ivy']
--- main.test.js
+++ main.test.js
@@ -4,7 +4,7 @@ import { dogMonthlyCost } from "./main.js";
 //Please see JamBoard following the link: for the examples
 // https://jamboard.google.com/d/1MyuUcj8szgu60FB7zdGt6VJepWAFEB5ADviME_Lpc8c/viewer?f=0
 
-// `dogMonthlyCost(['sticks', 2 , 'balls', 100, 'Ivy'])` should return "My dog Ivy has 2 sticks, 100 balls. This month she has cost me £10"
+//Example 1
 test("should return £10 for example 1 MVP1", () => {
   //Arrange
   const expected =
@@ -15,6 +15,7 @@ test("should return £10 for example 1 MVP1", () => {
   expect(actual).toBe(expected);
 });
 
+//Example 2
 test("should return £10 for example 2 MVP1", () => {
   //Arrange
   const expected =
@@ -25,6 +26,7 @@ test("should return £10 for example 2 MVP1", () => {
   expect(actual).toBe(expected);
 });
 
+//Example 3
 test("should return £5 for example 3, MVP1", () => {
   //Arrange
   let expected =
@@ -35,6 +37,7 @@ test("should return £5 for example 3, MVP1", () => {
   expect(actual).toBe(expected);
 });
 
+//Example 4
 //Please note, the above examples all took PLURAL sticks/balls... this next test will test if just 1 stick is given
 test("test for example 4, if 1 stick is given", function () {
   //Arrange
@@ -44,4 +47,28 @@ test("test for example 4, if 1 stick is given", function () {
   const actual = dogMonthlyCost(["sticks", 1, "balls", 75, "Princess"]);
   //Assert
   expect(actual).toEqual(expected);
+});
+
+//Example 5
+//Test if 1 ball is needed and no sticks
+test("test for example 5", () => {
+  //Arrage
+  let expected =
+    "My dog Spooky has 0 sticks and 1 ball. This month they have cost me £0.20";
+  //Act
+  const actual = dogMonthlyCost(["sticks", 0, "balls", 1, "Spooky"]);
+  //Assert
+  expect(actual).toEqual(expected);
+});
+
+//Example 6
+//Test if there is not a whole integer cost
+test("testing example 6, if there is not a integer for the cost", () => {
+  //Arrange
+  let expected =
+    "My dog Sparkle has 1 stick and 78 balls. This month they have cost me £10.60";
+  //Act
+  const actual = dogMonthlyCost(["sticks", 1, "balls", 78, "Sparkle"]);
+  //Assert
+  expect(actual).to.equal(expected);
 });

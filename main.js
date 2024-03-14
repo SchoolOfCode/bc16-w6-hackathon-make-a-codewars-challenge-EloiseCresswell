f8579a232d28c2f28b9ae776c8344ab64e6ae1a1
Leigh and El <leighandel@Leighs-MacBook-Pro.local>
2024-03-14 16:26:23 +0000

    test 4, test and main.JS changes

--- main.js
+++ main.js
@@ -8,19 +8,26 @@
  * - a stick will actually bring you money! (You get £5 per whole stick!)
  * - a ball will cost you 20p to purchase
  *
+ *Please note, with this dog, you will also be loosing money, so they will always cost you!
+ *However, take into consideration what you may need to change if there was only 1 item *recieved or if no items were received...
  *
  * A few examples:
  *    `dogMonthlyCost(['sticks', 2 , 'balls', 100, 'Ivy'])` should return "My dog Ivy has 2 sticks and 100 balls. This month they have cost me £10"
  *    `dogMonthlyCost(['sticks', 0, 'balls', 50, 'Steve'])` should return "My dog Steve has 0 sticks and 50 balls. This month they have cost me £10"
  */
+
 export function dogMonthlyCost(array) {
   // Good luck!
+  let sticks = "sticks";
   const numberSticks = array[1];
+  if (numberSticks === 1) {
+    sticks = "stick";
+  }
   const costSticks = numberSticks * 5;
   const numberBalls = array[3];
   const costBalls = numberBalls * 0.2;
   const cost = costBalls - costSticks;
-  return `My dog ${array[4]} has ${numberSticks} sticks and ${numberBalls} balls. This month they have cost me £${cost}`;
+  return `My dog ${array[4]} has ${numberSticks} ${sticks} and ${numberBalls} balls. This month they have cost me £${cost}`;
 }
 
 //['sticks', 2 , 'balls', 100, 'Ivy']
--- main.test.js
+++ main.test.js
@@ -1,6 +1,9 @@
 import { test, expect } from "vitest";
 import { dogMonthlyCost } from "./main.js";
 
+//Please see JamBoard following the link: for the examples
+// https://jamboard.google.com/d/1MyuUcj8szgu60FB7zdGt6VJepWAFEB5ADviME_Lpc8c/viewer?f=0
+
 // `dogMonthlyCost(['sticks', 2 , 'balls', 100, 'Ivy'])` should return "My dog Ivy has 2 sticks, 100 balls. This month she has cost me £10"
 test("should return £10 for example 1 MVP1", () => {
   //Arrange
@@ -24,8 +27,21 @@ test("should return £10 for example 2 MVP1", () => {
 
 test("should return £5 for example 3, MVP1", () => {
   //Arrange
-  let expecte =
-    "My dog Simon has 2 sticks and 75 balls. This month, they have cost me £5";
+  let expected =
+    "My dog Simon has 2 sticks and 75 balls. This month they have cost me £5";
   //Act
+  const actual = dogMonthlyCost(["sticks", 2, "balls", 75, "Simon"]);
   //Assert
+  expect(actual).toBe(expected);
+});
+
+//Please note, the above examples all took PLURAL sticks/balls... this next test will test if just 1 stick is given
+test("test for example 4, if 1 stick is given", function () {
+  //Arrange
+  let expected =
+    "My dog Princess has 1 stick and 75 balls. This month they have cost me £10";
+  //Act
+  const actual = dogMonthlyCost(["sticks", 1, "balls", 75, "Princess"]);
+  //Assert
+  expect(actual).toEqual(expected);
 });

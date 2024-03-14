07516dc397427ae2efd8620d3e13dfe7e81ed30f
Leigh and El <leighandel@Leighs-MacBook-Pro.local>
2024-03-14 15:52:50 +0000

    added description of MVP1 to Main.Js

---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ * Hello ther challenger! Your task is to write a function named `monthlyDogCost` which takes in an array and returns the cost of the dog based on how many items it needs and returns a string with the name of the dog and the cost of their toys that month.
+ *
+ * The array
+ * ['item1', numberofItem1, 'item2', numberofItem2, 'name of dog']
+ *
+ * Cost of some of the items...
+ * - a stick will actually bring you money! (You get £5 per whole stick!)
+ * - a ball will cost you 20p to purchase
+ *
+ *
+ * A few examples:
+ *    `monthlyDogCost(['sticks', 2 , 'balls', 100, 'Ivy'])` should return "My dog Ivy has 2 sticks, 100 balls. This month she has cost me £10"
+ *    `monthlyDogCost(['sticks', 0, 'balls', 50, 'Steve'])` should return "My dog Steve has 0 sticks, 50 balls. This month she has cost me £10"
+ */
+export function transformLength(string) {
+  // Good luck!
+}
